7205e957ccae8c3314a625fc6c40fa5212f146da
Lee Jae-yeol <rijgndqw012@gmail.com>
2024-04-08 09:03:45 +0900

    Update parser_test.dart to handle parsing nested nodes more accurately. (#9)

--- test/utils/parser_test.dart
+++ test/utils/parser_test.dart
@@ -5,8 +5,23 @@ void main() {
   test('parses opening tag from stream', () async {
     final parser = HTMLParser('<div></div>');
 
-    final document = await parser.parse() as ElementNode;
-    expect(document.tag, "html");
-    expect((document.children.first as ElementNode).tag, "div");
+    final rootNode = await parser.parse() as ElementNode;
+    expect(rootNode.tag, "body");
+    expect((rootNode.children.first as ElementNode).tag, "div");
+  });
+
+  test("parses next node", () async {
+    final parser = HTMLParser('<div>dddd<p>aaa</p>ddd</div>');
+
+    final rootNode = await parser.parse() as ElementNode;
+    final domNodes = rootNode.flatten();
+
+    expect(domNodes.length, 6);
+    expect((domNodes[0] as ElementNode).tag, "body");
+    expect((domNodes[1] as ElementNode).tag, "div");
+    expect((domNodes[2] as TextNode).text, "dddd");
+    expect((domNodes[3] as ElementNode).tag, "p");
+    expect((domNodes[4] as TextNode).text, "aaa");
+    expect((domNodes[5] as TextNode).text, "ddd");
   });
 }
