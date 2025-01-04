0954654399e35fb32e9695be3fae615e1b2f3eae
malkoG <rijgndqw012@gmail.com>
2025-01-04 17:28:11 +0900

    Enhance HtmlRenderer: Enable clickable links using url_launcher package

--- lib/components/html_renderer.dart
+++ lib/components/html_renderer.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_widget_from_html_core/flutter_widget_from_html_core.dart';
+import 'package:url_launcher/url_launcher.dart';
 
 class HtmlRenderer extends StatelessWidget {
   final String html;
@@ -7,6 +8,20 @@ class HtmlRenderer extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return HtmlWidget(html);
+    return HtmlWidget(
+      html,
+      customWidgetBuilder: (element) {
+        if (element.localName == 'a') {
+          return GestureDetector(
+            onTap: () async {
+              final url = element.attributes['href'];
+              final uri = Uri.parse(url!);
+              launchUrl(uri);
+            },
+            child: Text(element.text, style: TextStyle(color: Colors.blue)),
+          );
+        }
+      },
+    );
   }
 }
