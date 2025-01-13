65bb797761321d87d80bd3e025213e4927f9d990
malkoG <rijgndqw012@gmail.com>
2025-01-14 00:09:42 +0900

    Add mention-only filter to notification page

--- lib/pages/notification_page.dart
+++ lib/pages/notification_page.dart
@@ -3,14 +3,43 @@ import 'package:fedi_pipe/components/notification_feed.dart';
 import 'package:fedi_pipe/models/mastodon_notification.dart';
 import 'package:flutter/material.dart';
 
-class NotificationPage extends StatelessWidget {
+class NotificationPage extends StatefulWidget {
   const NotificationPage({super.key});
+
+  @override
+  State<NotificationPage> createState() => _NotificationPageState();
+}
+
+class _NotificationPageState extends State<NotificationPage> {
+  NotificationFeedType selected = NotificationFeedType.all;
+  final List<NotificationFeedType> segments = [
+    NotificationFeedType.all,
+    NotificationFeedType.mention,
+  ];
 
   @override
   Widget build(BuildContext context) {
     return DefaultLayout(
-        body: NotificationFeed(
-          feedType: NotificationFeedType.all,
+        body: Column(
+          mainAxisSize: MainAxisSize.min,
+          children: [
+            SegmentedButton<NotificationFeedType>(
+                segments: segments
+                    .map((segment) => ButtonSegment<NotificationFeedType>(label: Text(segment.name), value: segment))
+                    .toList(),
+                selected: {selected},
+                onSelectionChanged: (selection) {
+                  setState(() {
+                    selected = selection.first;
+                  });
+                }),
+            Expanded(
+              child: NotificationFeed(
+                key: ValueKey(selected),
+                feedType: selected,
+              ),
+            ),
+          ],
         ),
         title: "Notifications");
   }
