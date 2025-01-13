2d9cb5942acdbe03f403ab7615d21f0b5579f7ea
malkoG <rijgndqw012@gmail.com>
2025-01-13 23:56:49 +0900

    Add MastodonNotificationRepository for handling notification API requests

---
+++ lib/repositories/mastodon/notification_repository.dart
@@ -0,0 +1,53 @@
+import 'dart:convert';
+
+import 'package:fedi_pipe/models/mastodon_notification.dart';
+import 'package:fedi_pipe/models/mastodon_status.dart';
+import 'package:fedi_pipe/repositories/mastodon/mastodon_base_repository.dart';
+
+/*  Example Link header:
+  link: <https://social.silicon.moe/api/v2/notifications?exclude_types%5B%5D=follow_request&grouped_types%5B%5D=favourite&grouped_types%5B%5D=reblog&grouped_types%5B%5D=follow&max_id=172698>; rel="next", <https://social.silicon.moe/api/v2/notifications?exclude_types%5B%5D=follow_request&grouped_types%5B%5D=favourite&grouped_types%5B%5D=reblog&grouped_types%5B%5D=follow&min_id=173515>; rel="prev"
+*/
+
+class MastodonNotificationRepository extends MastodonBaseRepository {
+  static Future<MastodonNotificationModel> fetchNotification(String id) async {
+    final response = await Client.get('/api/v1/notifications/$id');
+    final json = jsonDecode(response.body);
+    final status = MastodonNotificationModel.fromJson(json);
+
+    return status;
+  }
+
+  static Future<List<MastodonNotificationModel>> fetchNotifications(
+      {String? previousId, String? nextId, NotificationFeedType feedType = NotificationFeedType.all}) async {
+    final queryParameters = <String, String>{};
+    if (previousId != null) {
+      queryParameters['max_id'] = previousId;
+    } else if (nextId != null) {
+      queryParameters['min_id'] = nextId;
+    }
+
+    final path = "/api/v1/notifications";
+    switch (feedType) {
+      case NotificationFeedType.all:
+        break;
+      case NotificationFeedType.mentions:
+        queryParameters['types[]'] = 'mentions';
+        break;
+      case NotificationFeedType.favourites:
+        queryParameters['types[]'] = 'favourites';
+        break;
+      case NotificationFeedType.reblogs:
+        queryParameters['types[]'] = 'reblogs';
+        break;
+      case NotificationFeedType.follows:
+        queryParameters['types[]'] = 'follows';
+        break;
+    }
+
+    final response = await Client.get(path, queryParameters: queryParameters);
+    final json = jsonDecode(response.body);
+    final notifications = MastodonNotificationModel.fromJsonList(json);
+
+    return notifications;
+  }
+}
