5a775cfa62fa31ab40dfb37c8c4d3d350f254100
malkoG <rijgndqw012@gmail.com>
2025-01-31 21:56:48 +0900

    Add Mastodon profile bottom sheet for mention links in HTML renderer

--- lib/components/html_renderer.dart
+++ lib/components/html_renderer.dart
@@ -1,3 +1,4 @@
+import 'package:fedi_pipe/components/mastodon_profile_bottom_sheet.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_widget_from_html_core/flutter_widget_from_html_core.dart';
 import 'package:url_launcher/url_launcher.dart';
@@ -21,6 +22,26 @@ class HtmlRenderer extends StatelessWidget {
       },
       customWidgetBuilder: (element) {
         if (element.localName == 'a') {
+          if ((element.attributes['class'] ?? '').contains('mention')) {
+            final href = element.attributes['href'];
+            final text = element.text;
+
+            final urlHost = Uri.parse(href!).host;
+            final acct = "${text}@${urlHost}";
+
+            return GestureDetector(
+              onTap: () {
+                print("------");
+                print(acct);
+                print("------");
+                showMastodonProfileBottomSheetWithLoading(context, acct);
+              },
+              child: Text(element.text,
+                  style: TextStyle(
+                      color: Colors.blue, decoration: TextDecoration.underline, decorationColor: Colors.blue)),
+            );
+          }
+
           return InlineCustomWidget(
             child: GestureDetector(
               onTap: () async {
