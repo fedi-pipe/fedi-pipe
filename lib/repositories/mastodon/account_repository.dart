3021a1c2a17705da5608eb94572b0c06249c45c9
malkoG <rijgndqw012@gmail.com>
2025-05-18 22:42:55 +0900

    Refactor account lookup to handle remote and local accounts separately

--- lib/repositories/mastodon/account_repository.dart
+++ lib/repositories/mastodon/account_repository.dart
@@ -5,11 +5,46 @@ import 'package:fedi_pipe/repositories/mastodon/mastodon_base_repository.dart';
 
 class MastodonAccountRepository extends MastodonBaseRepository {
   static Future<MastodonAccountModel> lookUpAccount(String acct) async {
-    final response = await Client.get('/api/v1/accounts/lookup', queryParameters: {'acct': acct});
-    final json = jsonDecode(response.body);
-    final account = MastodonAccountModel.fromJson(json);
+    try {
+      if (acct.startsWith('@')) {
+        final account = await _tryLookUpRemoteAccount(acct);
+        if (account != null) {
+          return account;
+        }
+      }
+    } catch (e) {
+      print(e);
+    }
 
-    return account;
+    final localAccount = await _tryLookUpLocalAccount(acct);
+    return localAccount;
+  }
+
+  static Future<dynamic> _tryLookUpRemoteAccount(String acct) async {
+    try {
+      final response = await Client.get('/api/v1/accounts/lookup', queryParameters: {'acct': acct.substring(1)});
+      final json = jsonDecode(response.body);
+      final account = MastodonAccountModel.fromJson(json);
+
+      return account;
+    } catch (e) {
+      print(e);
+      return null;
+    }
+  }
+
+  static Future<dynamic> _tryLookUpLocalAccount(String acct) async {
+    final handle = acct.substring(1).split('@')[0];
+    try {
+      final response = await Client.get('/api/v1/accounts/lookup', queryParameters: {'acct': handle});
+      final json = jsonDecode(response.body);
+      final account = MastodonAccountModel.fromJson(json);
+
+      return account;
+    } catch (e) {
+      print(e);
+      return null;
+    }
   }
 
   static Future<List<MastodonAccountModel>> searchAccounts(String query) async {
